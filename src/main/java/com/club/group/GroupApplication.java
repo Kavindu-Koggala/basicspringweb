e3007a5c93b9a459fba934a7adba78700033e813
Kavindu Koggala <133685138+Kavindu-Koggala@users.noreply.github.com>
2023-12-14 21:02:34 +0530

    backend basic
    basic ones

---
+++ src/main/java/com/club/group/GroupApplication.java
@@ -0,0 +1,20 @@
+package com.club.group;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+}
